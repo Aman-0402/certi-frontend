42d69be36758a98e375fb6ed974777034d20c346
Aman <aman08.stars@gmail.com>
2026-07-09 22:41:14 +0530

    Add Your Journey to a Verified Credential section
    4-step process row (Choose Certification, Take Secure Assessment,
    Prove Your Skills, Earn Your Certificate) with numbered icon tiles
    and dashed connector arrows, placed below Certifications.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
 import Certifications from './components/Certifications'
+import JourneySteps from './components/JourneySteps'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <TrustedBy />
       <Certifications />
+      <JourneySteps />
     </div>
   )
 }
